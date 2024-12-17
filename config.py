0bc5834de864876538f2f1351e65f8b4ab7f23e7
Ivan <84268076+operrl@users.noreply.github.com>
2024-12-17 23:53:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
-token = 'MTA1MzM2ODQ4OTMzNjg5NzY0Ng.GN-M2o.SrKsqshaxj5wcI9ZQofseJW0N4KSpTqxhmNZIw'
-meme = ['E:\bot\furion.mp3', 'E:\bot\bear.mp3', 'E:\bot\anapa).mp3']
-memef5 = []
-memeall = meme + memef5
-a = len(meme)
-a -= 1
+token = 'ur token here'
+
